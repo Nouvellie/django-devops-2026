4357c8fb9f22728f2eae14836ad6aed83e424789
rocuant <rocuantnouve+gthb@gmail.com>
2026-09-14 17:06:25 -0300

    first version

--- devops/wsgi.py
+++ devops/wsgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edenthought.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devops.settings")
 
 
 def application(environ, start_response):
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn..."
-exec gunicorn edenthought.wsgi:application --bind 0.0.0.0:8000 --workers 3
+exec gunicorn devops.wsgi:application --bind 0.0.0.0:8000 --workers 3
