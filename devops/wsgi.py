cc9aa48a2ce598d37002cec29694db221694b51c
rocuant <rocuantnouve+gthb@gmail.com>
2026-09-14 16:56:17 -0300

    first version

---
+++ devops/wsgi.py
@@ -0,0 +1,14 @@
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edenthought.settings")
+
+
+def application(environ, start_response):
+
+    for key, value in environ.items():
+        if isinstance(value, str):
+            os.environ[key] = value
+
+    return get_wsgi_application()(environ, start_response)
